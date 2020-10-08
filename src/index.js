2111eea131120dd1d3d2ea22588ee1f094a15475
Levendor <myth_of_lavanda@mail.ru>
2020-10-08 22:39:18 +0300

    init: solve towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!Array.isArray(matrix)) return [];
+	let n = 0;
+	return matrix.reduce((accum, item, index) => {
+		if (index === 2*n){
+			n++;
+			return accum.concat(item)
+		} else {
+			return accum.concat(item.reverse())			
+		};
+	}, []);	
 }
+
+// module.exports = function towelSort (matrix) {
+//   return [];
+// }
